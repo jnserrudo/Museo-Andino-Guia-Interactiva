452c43a7624304b554ff79fb46ae07af87e1ca1f
José Nahuel Serrudo <jnserrudo@ucasal.local>
2024-12-12 15:36:41 -0300

    fix

--- museo-andino-guia-interactiva/vite.config.js
+++ museo-andino-guia-interactiva/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/museo",
   build: {
     outDir: "../museo-andino-guia-interactiva/dist",
     rollupOptions: {
