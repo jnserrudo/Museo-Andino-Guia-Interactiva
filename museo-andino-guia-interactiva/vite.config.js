8b56852f27398974af7bc3509829d52cb98f67d3
José Nahuel Serrudo <jnserrudo@ucasal.local>
2024-12-12 12:45:14 -0300

    vite config

--- museo-andino-guia-interactiva/vite.config.js
+++ museo-andino-guia-interactiva/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  build: {
+    outDir: "../museo-andino-guia-interactiva/dist",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+});
